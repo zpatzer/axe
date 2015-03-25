93f397e021be2f1bb0bc900857dc6c0f69a403ee
zpatzer <zpatzer@gmail.com>
2015-03-24 23:13:41 -0500

    libgdx

---
+++ desktop/src/com/BSGaxe/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.BSGaxe.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.BSGaxe.game.axe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new axe(), config);
+	}
+}
